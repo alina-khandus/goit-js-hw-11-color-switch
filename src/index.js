9166182f881cadf6fc0c9fe7751138c4bb62cbbd
Alina Khandus <alinakhandus@gmail.com>
2020-11-29 13:14:32 +0200

    ДЗ

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { onStartClick, onStopClick } from './js/generator_colors.js';
+import './styles.css';
+
+
+
+refs.buttonStart.addEventListener('click', onStartClick);
+refs.buttonStop.addEventListener('click', onStopClick);
